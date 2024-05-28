01abda6e879529529501b1e609be39a812463605
Viktor Tarutin <viktor_tarutin@mail.ru>
2024-05-28 16:59:17 +0300

    Fix usersMigration commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->enum('sex', ['male', 'female'])->nullable();
+            $table->string('age')->nullable();
+            $table->string('address')->nullable();
+            $table->date('dateOfBirth')->nullable();
+            $table->string('email')->nullable();
             $table->string('google_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
